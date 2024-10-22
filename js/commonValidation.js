88738e0682d641349eb923918e217d9b9758212d
purusharth-soni <sonipurusharth@gmail.com>
2024-10-22 14:06:36 -0500

    WPL-001: Added cars page and validation

---
+++ js/commonValidation.js
@@ -0,0 +1,63 @@
+const validCities = [
+  // Texas
+  "Austin",
+  "Dallas",
+  "Houston",
+  "San Antonio",
+  "Fort Worth",
+  "El Paso",
+  "Arlington",
+  "Corpus Christi",
+  "Plano",
+  "Lubbock",
+  "McAllen",
+  "Amarillo",
+  "Brownsville",
+  "Waco",
+  "Killeen",
+  "Midland",
+  "Odessa",
+  "Harlingen",
+  "Laredo",
+  "Beaumont",
+  "College Station",
+  "Wichita Falls",
+  "Longview",
+  "Abilene",
+  "Tyler",
+
+  // California
+  "Los Angeles",
+  "San Francisco",
+  "San Diego",
+  "San Jose",
+  "Sacramento",
+  "Fresno",
+  "Oakland",
+  "Santa Ana",
+  "Long Beach",
+  "Burbank",
+  "Palm Springs",
+  "Ontario",
+  "Santa Barbara",
+  "Bakersfield",
+  "Riverside",
+  "Stockton",
+  "Modesto",
+  "San Bernardino",
+  "Redding",
+  "Santa Rosa",
+  "Oxnard",
+  "Visalia",
+  "Chico",
+  "Monterey",
+  "Eureka",
+];
+
+function isValidCity(city) {
+  if (!city || city.trim() === "") return false;
+
+  return (
+    validCities.map((c) => c.toLowerCase()).indexOf(city.toLowerCase()) !== -1
+  );
+}
